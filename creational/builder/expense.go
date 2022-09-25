91a66be8503ad14395ef0876aabfd641e9010f5f
Firmino Changani <firmino.changani@gmail.com>
2022-09-25 01:06:27 +0100

    add an expense builder

--- creational/builder/expense.go
+++ creational/builder/expense.go
@@ -1,1 +1,91 @@
 package main
+
+import (
+	"errors"
+	"time"
+)
+
+type Expense struct {
+	id           string
+	date         time.Time
+	attachment   string
+	notes        string
+	amount       float32
+	amountTax    float32
+	currencyCode string
+	status       string
+}
+
+type ExpenseBuilder struct {
+	Id           string
+	Date         time.Time
+	Attachment   string
+	Notes        string
+	Amount       float32
+	AmountTax    float32
+	CurrencyCode string
+	Status       string
+}
+
+func NewExpenseBuilder() *ExpenseBuilder {
+	return &ExpenseBuilder{}
+}
+
+func (b *ExpenseBuilder) SetId(id string) {
+	b.Id = id
+}
+
+func (b *ExpenseBuilder) SetDate(date time.Time) {
+	b.Date = date
+}
+
+func (b *ExpenseBuilder) SetAttachment(attachment string) {
+	b.Attachment = attachment
+}
+
+func (b *ExpenseBuilder) SetNotes(notes string) {
+	b.Notes = notes
+}
+
+func (b *ExpenseBuilder) SetAmount(amount float32) {
+	b.Amount = amount
+}
+
+func (b *ExpenseBuilder) SetAmountTax(amountTax float32) {
+	b.AmountTax = amountTax
+}
+
+func (b *ExpenseBuilder) SetCurrencyCode(currencyCode string) {
+	b.CurrencyCode = currencyCode
+}
+
+func (b *ExpenseBuilder) SetStatus(status string) {
+	b.Status = status
+}
+
+func (b *ExpenseBuilder) Build() (*Expense, error) {
+
+	// if b.Status != "pending" || b.Status != "approved" || b.Status != "rejected" {
+	if b.Status == "" {
+		return nil, errors.New("status must be: pending, approved or rejected")
+	}
+
+	if b.Amount <= 0 {
+		return nil, errors.New("amount must be greater than 0")
+	}
+
+	if b.AmountTax < 0 {
+		return nil, errors.New("amountTax must be a float between 0.0 and 1.0")
+	}
+
+	return &Expense{
+		id:           b.Id,
+		date:         b.Date,
+		attachment:   b.Attachment,
+		notes:        b.Notes,
+		amount:       b.Amount,
+		amountTax:    b.AmountTax,
+		currencyCode: b.CurrencyCode,
+		status:       b.Status,
+	}, nil
+}
--- creational/builder/expense_test.go
+++ creational/builder/expense_test.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestExpenseBuilder_Build(t *testing.T) {
+	b := NewExpenseBuilder()
+	b.SetStatus("pending")
+	expense, err := b.Build()
+
+	assert.NotNil(t, err, "expense cannot be created without values")
+	assert.Nil(t, expense)
+
+	b = NewExpenseBuilder()
+	b.SetId("some-unique-id")
+	b.SetStatus("pending")
+	b.SetAmount(100)
+	b.SetAmountTax(0)
+	b.SetCurrencyCode("USD")
+
+	expense, err = b.Build()
+
+	assert.Nil(t, err)
+	assert.NotNil(t, expense)
+}
